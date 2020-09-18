14f75ca9f698c67587a767229e9e5a962e3976c2
Risegek <dimka-tarkov@yandex.ru>
2020-09-18 17:07:31 +0700

    Realized most of methods. Added menu

--- Lab1/Lab1/List.h
+++ Lab1/Lab1/List.h
@@ -3,31 +3,17 @@ template <class T>
 class List
 {
 private:
-	int size;
-	int capacity0;
-	int capacity;
+	int capacity0=0;
+	int size=0;
+	int capacity=0;
 	T* array;
-	int seenValCount;
+	int seenValCount=0;
 
 public:
-	List();
-	List(int capacity0);
-	~List();
-	List(const List<T>& list);
+	
+	//List(const List<T>& list);
 
-	int getSize();
-	void clear();
-	bool isEmpty();
-	bool contains(T value);
-	T get(int index);
-	void set(int index, T value);
-	int getIndex(T value);
-	void add(T value);
-	bool add(int index, T value);
-	bool deleteValue(T value);
-	bool deleteIndexVal(int index);
-	int getSeenValCount();
-	void show();
+
 
 	class Iterator
 	{
@@ -51,8 +37,187 @@ public:
 
 	friend class Iterator;
 	friend class reverse_Iterator;
+
+
 	Iterator begin();
 	reverse_Iterator rbegin();
 	Iterator end();
 	reverse_Iterator rend();
+
+
+
+
+	List() {
+		capacity0 = capacity = 2;
+		size = 0;
+		array = new T[capacity0];
+	}
+
+	List(int capacity0) {
+		this->capacity0 = capacity = capacity0;
+		size = 0;
+		array = new T[capacity0];
+	}
+	~List() {
+		delete[] array;
+	}
+
+	List(const List<T>& list) {
+
+
+
+	}
+
+
+
+	int getSize() { return size ;};
+
+	void clear() {
+		T* copyArr = new T[capacity0];
+
+		delete[] array;
+		array = copyArr;
+		delete copyArr;
+		size = 0;
+		capacity = capacity0;
+	}
+
+
+	bool isEmpty() {
+		if (size == 0) return true;
+		else return false;
+	}
+
+
+	bool contains(T value) {
+		for (int i = 0; i < size; i++) {
+			seenValCount++;
+			if (array[i] == value) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+
+	T get(int index) {
+		if (index > size); //добавить исключение
+		else return array[index];
+	}
+
+
+	void set(int index, T value) {
+		if (index > size);  //добавить исключение
+		else array[index] = value;
+	}
+
+
+	int getIndex(T value) {
+		for (int i = 0; i < size; i++) 
+			if (array[i] == value)return i;
+
+		return -1;
+	}
+
+
+	void add(T value) {
+		array[size] = value;
+		size++;
+		if (size == capacity) {
+			capacity *= 2;
+			T* copyArr = new T[capacity];
+			for (int i = 0; i < size; i++) {
+				copyArr[i] = array[i];
+			}
+			delete[] array;
+			array = copyArr;
+		}
+	}
+
+	//void add(int index, T value) {
+	//	if (index > size)return false;
+	//	else {
+	//
+	//
+	//	}
+	//
+	//} If your read this pls send me some letter in VK
+
+	bool add(int index, T value) {
+		if (index > size)return false;
+		else {
+			for (int i = size; i > index; i--) {
+				array[i] = array[i - 1];
+			}
+			array[index] = value;
+			size++;
+			if (size == capacity) {
+				capacity *= 2;
+				T* copyArr = new T[capacity];
+				for (int i = 0; i < size; i++) {
+					copyArr[i] = array[i];
+				}
+				delete[] array;
+				array = copyArr;
+			}
+
+			return true;
+		}
+	}
+
+	bool deleteValue(T value) {
+		bool isFind = false;
+		for (int i = 0; i < size; i++) {
+			if (array[i] == value) {
+				isFind = true;
+			}
+			if (isFind) {
+				if (i == size-1) break;
+				array[i] = array[i + 1];
+			}
+		}
+
+		size--;
+		if (size == capacity / 2) {
+			capacity = size + capacity0;
+			T* copyArr = new T[capacity];
+			for (int i = 0; i < size; i++) {
+				copyArr[i] = array[i];
+			}
+			delete[] array;
+			array = copyArr;
+		}
+
+		return isFind;
+	}
+
+	bool deleteIndexVal(int index) {
+		if (index > size) return false;
+		else {
+			for (int i = index; i < size - 1; i++) {
+				array[i] = array[i + 1];
+			}
+
+			size--;
+			if (size == capacity / 2) {
+				capacity = size + capacity0;
+				T* copyArr = new T[capacity];
+				for (int i = 0; i < size; i++) {
+					copyArr[i] = array[i];
+				}
+				delete[] array;
+				array = copyArr;
+			}
+
+			return true;
+		}
+
+	}
+
+	int getSeenValCount() {
+		return seenValCount;
+	}
+
+
+	
 };
--- Lab1/Lab1/Source.cpp
+++ Lab1/Lab1/Source.cpp
@@ -1,1 +1,75 @@
 //2 вариант
+#include <iostream>
+#include <locale.h>
+#include <String>
+#include "List.h"
+
+int main() {
+	setlocale(LC_ALL, "Rus");
+	List<int> list;
+	bool n = true;
+	while (n) {
+		int k;
+		std::cout << "\t\tMenu\n" << "1. Добавить элемент в массив\n" << "2. Добавить элемент по номеру\n"
+			<< "3. Размер массива\n" << "4.Удалить элемент\n" << "5. Удалить элемент по номеру\n"
+			<< "6.Очистить массив\n" << "7. Проверить на пустоту\n" << "8. Проверить, есть ли элемент в массиве\n"
+			<< "9. Изменить значение по номеру\n" << "10. Вывести значение по номеру\n" << "11. Вывести позицию заданного элемента\n"
+			<< "12. Вывести массив на экран\n" << "13. Число просмотренных элементов\n" << "14. ИТЕРАТОРЫ(ПОТОМ)\n";
+		std::cin >> k;
+
+		switch (k)
+		{
+		case 1: {
+			int value;
+			std::cout << "Введите значение: ";
+			std::cin >> value;
+			list.add(value);
+			std::cout << std::endl;
+			break; }
+		case 2: {
+			int value,index;
+			std::cout << "Введите индекс и значение: ";
+			std::cin  >> index >> value;
+			std::cout << list.add(index,value);
+			std::cout << std::endl;
+			break; }
+		case 3: {
+			std::cout << list.getSize() << std::endl;
+			break; }
+		case 4: {
+			break; }
+		case 5: {
+			break; }
+		case 6: {
+			break; }
+		case 7: {
+			break; }
+		case 8: {
+			break; }
+		case 9: {
+
+			break; }
+		case 10: {break; }
+		case 11: {break; }
+		case 12: {
+			int size = list.getSize();
+			for (int i = 0; i < size; i++) {
+				std::cout << list.get(i) << " ";
+			}
+			std::cout << std::endl;
+
+			break; }
+		case 13: {break; }
+		case 14: {break; }
+		case 15: {break; }
+		default:
+			break;
+		}
+
+
+	}
+
+
+
+	return 0;
+}
